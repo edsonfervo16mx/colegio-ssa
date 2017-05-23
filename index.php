df692e20c91ceaa063d6e699b6972c56a26d3f10
edsonfervo16mx <edson55_@hotmail.com>
2017-05-23 07:00:36 -0500

    diseno previo de la interfaz

--- index.php
+++ index.php
@@ -4,28 +4,92 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>Jean Piaget - SSA</title>
+	<link rel="stylesheet" href="assets/css/main.css">
 	<link rel="stylesheet" href="assets/css/materialize.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-	<div class="container">
-		<h1>Hola mundo</h1>
-		<i class="material-icons">add</i>
+	<div class="container area">
+		<!--<h3 class="grey-text lighten-2 center-align">Colegio Jean Piaget</h1>-->
 		<div class="row">
-        <div class="col s12 m6">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Card Title</span>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
-      </div>
+			<div class="col s12 m6">
+				<div class="card">
+					<div class="card-image">
+						<img src="assets/images/img-card-primary.jpg">
+						<span class="card-title">Colegio Jean Piaget</span>
+					</div>
+					<div class="card-content">
+						<form action="" method="POST">
+							<div class="row">
+								<div class="input-field col s10 offset-s1">
+									<input type="text" name="usuario" id="usuario" placeholder="Usuario">
+									<label for="usuario">Nombre de Usuario</label>
+								</div>
+								<div class="input-field col s10 offset-s1">
+									<input type="password" name="pass" id="pass" placeholder="Contraseña">
+									<label for="pass">Contraseña de Usuario</label>
+								</div>
+								<div class="input-field col s10 offset-s1 right-align">
+									<input type="submit" value="Entrar" class="waves-effect waves-light btn">
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m6">
+				<div class="row">
+					<div class="col s6">
+						<div class="card-panel light-blue darken-2 center-align">
+							<span class="white-text">
+								<i class="large material-icons">perm_identity</i>
+								<h6>Alumnos</h6>
+							</span>
+						</div>
+					</div>
+					<div class="col s6">
+						<div class="card-panel teal center-align">
+							<span class="white-text">
+								<i class="large material-icons">input</i>
+								<h6>Inscripciones</h6>
+							</span>
+						</div>
+					</div>
+					<div class="col s6">
+						<div class="card-panel light-green darken-1 center-align">
+							<span class="white-text">
+								<i class="large material-icons">assignment_ind</i>
+								<h6>Colegiaturas</h6>
+							</span>
+						</div>
+					</div>
+					<div class="col s6">
+						<div class="card-panel yellow darken-2 center-align">
+							<span class="white-text">
+								<i class="large material-icons">shopping_cart</i>
+								<h6>Ventas</h6>
+							</span>
+						</div>
+					</div>
+					<div class="col s6">
+						<div class="card-panel deep-purple darken-1 center-align">
+							<span class="white-text">
+								<i class="large material-icons">assessment</i>
+								<h6>Estadisticas</h6>
+							</span>
+						</div>
+					</div>
+					<div class="col s6">
+						<div class="card-panel pink darken-1 center-align">
+							<span class="white-text">
+								<i class="large material-icons">assignment</i>
+								<h6>Reportes</h6>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<script src="assets/js/jquery.js"></script>
 	<script src="assets/js/materialize.js"></script>
