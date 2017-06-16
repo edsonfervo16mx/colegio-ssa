db99cb120f91b90f31f2d2bc8f20d8bcca0a52a2
edsonfervo16mx <edson55_@hotmail.com>
2017-06-16 07:23:49 -0500

    detalle con la cadena de conexion

--- web/config/key.connection.php
+++ web/config/key.connection.php
@@ -2,14 +2,6 @@
 	session_start();
 	date_default_timezone_set('Mexico/General');
 
-	/*
-	$key = array(
-		'host' => 'localhost',
-		'user' => 'iclounde_admjp1',
-		'pass' => 'F3rnando16',
-		'db' => 'iclounde_school_jean_piaget' 
-	);
-	/**/
 
 	$key = array(
 		'host' => 'localhost',
